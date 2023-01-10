df1f366fe31960247ddc42b2d7ba10232843a996
Altug B. Altintas <altuga@gmail.com>
2023-01-10 12:53:52 +0300

    project added

---
+++ README.md
@@ -0,0 +1,1 @@
+# BDD basit örneği içerir
---
+++ src/test/java/com/kodcu/is/HisseSenediIslemleriSteps.java
@@ -0,0 +1,40 @@
+
+package com.kodcu.is;
+
+
+import io.cucumber.java.tr.Diyelimki;
+import io.cucumber.java.tr.Eğerki;
+import io.cucumber.java.tr.Ozaman;
+
+public class HisseSenediIslemleriSteps {
+
+    private HisseService hisseServisi;
+    private HisseSenet hisse;
+
+
+
+
+    @Diyelimki("^hisse senedinin birim eşik satış fiyatı (\\d+)$")
+    public void hisseSenedininBirimEşikSatışFiyatı(int esikDeger) throws Throwable {
+        hisseServisi = new HisseService();
+        hisse = hisseServisi.yeniHisseEkle("STK", esikDeger);
+    }
+
+    @Eğerki("^hisse senedi (\\d+) üzerinden işlem görüyorsa$")
+    public void hisseSenediÜzerindenIşlemGörüyorsa(int fiyat) throws Throwable {
+        hisse.islem(fiyat);
+    }
+
+    @Ozaman("^sistem  uyarısı  şöyle olmalıdır OFF$")
+    public void sistemUyarısıŞöyleOlmalıdırOFF() throws Throwable {
+
+        org.junit.Assert.assertEquals(hisse.getDurum(), "OFF");
+    }
+
+    @Ozaman("^sistem  uyarısı  şöyle olmalıdır ON$")
+    public void sistemUyarısıŞöyleOlmalıdırON() throws Throwable {
+        org.junit.Assert.assertEquals(hisse.getDurum(), "ON");
+    }
+
+
+}
